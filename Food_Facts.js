7c6b013a9e7f624d0e30961d85b89862c537af66
Gurucharan rao <54024246+superslimguru@users.noreply.github.com>
2020-05-22 16:43:34 +0530

    Update Food_Facts.js

--- Food_Facts.js
+++ Food_Facts.js
@@ -1,6 +1,4 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
+
 const Alexa = require('ask-sdk-core');
 const data = [
   'Green, yellow, and red bell peppers are not actually the same vegetable.',
